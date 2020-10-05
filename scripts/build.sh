df0004d94897b85cfef7af227af281e8b968a4fb
Colbyn Wadman <hello@colbyn.com>
2020-10-05 12:59:32 -0600

    latex music notation isn't working that well

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,15 @@
-set -e
+function errorlog {
+    echo "\033[1;31m\033[4m$1\033[0m"
+}
 
 mkdir -p output
-xelatex --interaction=batchmode -output-directory output source/main.tex
+cd source
+
+xelatex --interaction=batchmode -output-directory ../output main.tex >/dev/null
+STATUS=$?
+
+if [ "$STATUS" -ne "0" ]; then
+    errorlog "BUILD FAILED:"
+    xelatex --interaction=nonstopmode -output-directory ../output main.tex
+    errorlog "END OF ERROR MESSAGE"
+fi
